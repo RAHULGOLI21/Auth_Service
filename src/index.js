17e77f2cf19cd0e5d667be7aba2c3b8b9a0fae47
Rahul <golirahul21@gmail.com>
2024-05-13 11:15:27 +0530

    Added Roles and User_Roles through tables

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 
 const app = express();
-
-const UserService = require('./services/user-service')
+const db = require('./models/index')
 
 prepareAndStartServer = () => {
 
@@ -17,13 +16,9 @@ prepareAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at PORT : ${PORT}`);
-        // code snippet to verify token
-        // const service = new UserService();
-        // // const token = service.createToken({ email: 'rahulgoli21@gmail.com', id: 1 })
-        // // console.log(token);
-        // const res = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhaHVsZ29saTIxQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE3MTU0NDI0MTksImV4cCI6MTcxNTQ0MjQxOX0.ZoX_o6_OYtPCAuVTMUwNQwwWXii6N-OjFG0_1A7EWeI'
-        // const isTrue = service.verifyToken(res);
-        // console.log(isTrue);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true})
+        }
     })
 
 
